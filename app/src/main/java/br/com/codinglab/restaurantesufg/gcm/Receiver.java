1cdeb3a61ce7c949339b4548749b4b128f1e74a5
Samuel Junio de Almeida <almeida.samuel.junio@gmail.com>
2015-07-01 23:59:51 -0300

    Implementação GCM
    Implementação do registro do dispositivo para com o aplicativo
    RestaurantesUFG usando GCM.

---
+++ app/src/main/java/br/com/codinglab/restaurantesufg/gcm/Receiver.java
@@ -0,0 +1,23 @@
+package br.com.codinglab.restaurantesufg.gcm;
+
+import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.WakefulBroadcastReceiver;
+
+/**
+ * Created by PC MASTER RACE on 01/07/2015.
+ */
+
+public class Receiver extends WakefulBroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // Explicitly specify that GcmIntentService will handle the intent.
+        ComponentName comp = new ComponentName(context.getPackageName(),  Receiver.class.getName());
+        // Start the service, keeping the device awake while it is launching.
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
+
--- app/src/main/java/br/com/codinglab/restaurantesufg/main/MainActivity.java
+++ app/src/main/java/br/com/codinglab/restaurantesufg/main/MainActivity.java
@@ -1,5 +1,10 @@
 package br.com.codinglab.restaurantesufg.main;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
@@ -11,7 +16,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
+import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
@@ -20,6 +32,20 @@ import br.com.codinglab.restaurantesufg.modelos.Restaurante;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    public static final String EXTRA_MESSAGE = "message";
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+
+
+    GoogleCloudMessaging gcm;
+    String regId;
+    Context context;
+    String SENDER_ID = "126111843784";
+
+    static final String TAG = "GCM Demo";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +56,18 @@ public class MainActivity extends ActionBarActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new InicioFragment())
                     .commit();
+        }
+
+        if(checkPlayServices()){
+            context = getApplicationContext();
+            gcm = GoogleCloudMessaging.getInstance(this);
+            regId = getRegistrationId(context);
+            if(regId.isEmpty()){
+                registerInBackground();
+            }
+            Log.i(TAG, "Registered: " + regId);
+        }else{
+            Log.i(TAG, "No valid Google Play Services APK found.");
         }
     }
 
@@ -100,5 +138,114 @@ public class MainActivity extends ActionBarActivity {
 
             return rootView;
         }
+
     }
+
+
+
+
+
+
+    /*CGM*/
+    private boolean checkPlayServices(){
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(resultCode != ConnectionResult.SUCCESS){
+            if(GooglePlayServicesUtil.isUserRecoverableError(resultCode)){
+                GooglePlayServicesUtil.getErrorDialog(resultCode,this,PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            }else{
+                Toast.makeText(this,"Dispositivo não suportado",Toast.LENGTH_LONG);
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private String getRegistrationId(Context context) {
+
+        final SharedPreferences prefs = getGcmPreferences(context);
+
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        // Check if app was updated; if so, it must clear the registration ID
+        // since the existing regID is not guaranteed to work with the new
+        // app version.
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            // should never happen
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+    private SharedPreferences getGcmPreferences(Context context) {
+        // This sample app persists the registration ID in shared preferences, but
+        // how you store the regID in your app is up to you.
+        return getSharedPreferences(MainActivity.class.getSimpleName(),Context.MODE_PRIVATE);
+    }
+    private void registerInBackground() {
+
+        new AsyncTask<Void, Void, String>() {
+            @Override
+            protected String doInBackground(Void... params) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(context);
+                    }
+                    regId = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regId;
+
+                    // You should send the registration ID to your server over HTTP, so it
+                    // can use GCM/HTTP or CCS to send messages to your app.
+                    sendRegistrationIdToBackend();
+
+                    // For this demo: we don't need to send it because the device will send
+                    // upstream messages to a server that echo back the message using the
+                    // 'from' address in the message.
+
+                    // Persist the regID - no need to register again.
+                    storeRegistrationId(context, regId);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                    // If there is an error, don't just keep trying to register.
+                    // Require the user to click a button again, or perform
+                    // exponential back-off.
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                //mDisplay.append(msg + "\n");
+            }
+        }.execute(null, null, null);
+
+
+    }
+    private void sendRegistrationIdToBackend() {
+        // Your implementation here.
+    }
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGcmPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+    /*END-CGM*/
 }
